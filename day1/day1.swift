1d6fbf3160991e18d360fb84b84647b8cf2631d9
vcosmin2701 <v.cosmin2717@gmail.com>
2024-12-01 15:33:46 +0200

    day1 implementation for challenge [WIP]

--- day1/day1.swift
+++ day1/day1.swift
@@ -22,8 +22,34 @@ do {
         }
     }
     
-    print("List 1: \(list1)")
-    print("List 2: \(list2)")
+    var totalDistance = 0
+    
+    let arr1 = list1.sorted()
+    let arr2 = list2.sorted()
+    
+    for i in 0..<arr1.count {
+        let distance = abs(arr1[i] - arr2[i])
+        totalDistance += distance
+    }
+    
+    print(totalDistance)
+    
+    var similarityScore = 0
+    
+    for num in list1 {
+        var count = 0
+        
+        for num1 in list2 {
+            if num1 == num {
+                count += 1
+            }
+        }
+        similarityScore += num * count
+    }
+    
+    print("Similarity: \(similarityScore)")
+
+    
 } catch {
     print("Error reading file: \(error)")
 }
